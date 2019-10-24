1bfadfb03e0cd79892a9e0770086dc6b22b3a245
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2019-10-24 13:39:57 -0500

    Solved the shrinking issue

--- src/components/product.js
+++ src/components/product.js
@@ -1,26 +1,37 @@
 import React from 'react'
 
-import {Anchor, Box, Button, Text} from 'grommet'
-import {Attraction} from 'grommet-icons'
+import {Anchor, Box, Button, Text, Image} from 'grommet'
+import {Cart} from 'grommet-icons'
 
 class Product extends React.Component {
     state = {
-
+        product: {
+            name: "Rubber Duck",
+            price: 12.50,
+            description: "An amazing rubber duck, 10x10.",
+            manufacturer: "B-Duck",
+            imageurl: ""
+        }
     }
 
     render() {
         return (
             <Box
-                pad="large"
+                wrap={true}
+                pad="medium"
                 align="center"
+                direction="column"
                 background={{color: "light-2", opacity: "strong"}}
                 round
                 gap="small"
                 animation="fadeIn">
-                    <Attraction size="large"/>
-                    <Text> Party </Text>
-                    <Anchor href="" label="link"/>
-                    <Button label="Button" onClick={() => {}}/>
+                    <Box width="xxsmall" height="xxsmall">
+                        <Image fit="contain" fallback="http://pngimg.com/uploads/rubber_duck/rubber_duck_PNG4.png" src={this.state.product.imageurl}/>
+                    </Box>
+                    <Text> {this.state.product.name} </Text>
+                    <Text size="small">{this.state.product.manufacturer}</Text>
+                    <Anchor href="#" label="See more"/>
+                    <Button icon={<Cart/>}label="Add to Cart" onClick={() => {}}/>
             </Box>
         )
     }
--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,7 +1,87 @@
 import React from 'react'
 
 import Product from '../components/product'
-import {Box,Heading, Grid} from 'grommet'
+import { Box, Heading, Grid, ResponsiveContext } from 'grommet'
+
+const columns = {
+    small: ["auto"],
+    medium: ["auto", "auto"],
+    large: ["auto", "auto", "auto"],
+    xlarge: ["auto", "auto", "auto"]
+}
+
+const rows = {
+    small: ["small", "small", "small"],
+    medium: ["small", "small"],
+    large: ["small"],
+    xlarge: ["small"]
+}
+
+const areas = {
+    small: [
+        { name: "header", start: [0, 0], end: [0, 0] },
+        { name: "test", start: [0, 1], end: [0, 1] },
+        { name: "test1", start: [0, 2], end: [0, 2] }
+    ],
+    medium: [
+        { name: "header", start: [0, 0], end: [1, 0] },
+        { name: "test", start: [0, 1], end: [0, 1] },
+        { name: "test1", start: [1, 1], end: [1, 1] }
+    ],
+    large: [
+        { name: "header", start: [0, 0], end: [0, 0] },
+        { name: "test", start: [1, 0], end: [1, 0] },
+        { name: "test1", start: [2, 0], end: [2, 0] }
+    ],
+    xlarge: [
+        { name: "header", start: [0, 0], end: [0, 0] },
+        { name: "test", start: [1, 0], end: [1, 0] },
+        { name: "test1", start: [2, 0], end: [2, 0] }
+    ]
+}
+
+const ResponsiveGrid = ({
+    children,
+    overrideColumns,
+    overrideRows,
+    areas,
+    ...props
+}) => (
+        <ResponsiveContext.Consumer>
+            {size => {
+                // take into consideration if not array is sent but a simple string
+                let columnsVal = columns;
+                if (columns) {
+                    if (columns[size]) {
+                        columnsVal = columns[size];
+                    }
+                }
+
+                let rowsVal = rows;
+                if (rows) {
+                    if (rows[size]) {
+                        rowsVal = rows[size];
+                    }
+                }
+
+                // also if areas is a simple array not an object of arrays for different sizes
+                let areasVal = areas;
+                if (areas && !Array.isArray(areas)) areasVal = areas[size];
+
+                return (
+                    <Grid
+                        {...props}
+                        areas={!areasVal ? undefined : areasVal}
+                        rows={!rowsVal ? size : rowsVal}
+                        columns={!columnsVal ? size : columnsVal}
+                    >
+                        {children}
+                    </Grid>
+                );
+            }}
+        </ResponsiveContext.Consumer>
+    )
+
 
 const style = {
     heading: {
@@ -11,45 +91,44 @@ const style = {
 
 class Catalog extends React.Component {
     state = {
-        recentProducts : [],
-        otherProducts : []
+        recentProducts: [],
+        otherProducts: []
     }
 
     componentWillMount = () => {
-        this.setState({recentProducts : [1,2,3,4], otherProducts : [5,6,7,8,9]})
+        this.setState({ recentProducts: [1, 2, 3, 4], otherProducts: [5, 6, 7, 8, 9,10] })
     }
 
     render() {
         return (
             <>
-            {/** 4 Most Recent Products */}
-            <Heading level="2" style={style.heading}>Most Recent Products</Heading>
-            <Box
-                direction="row-responsive"
-                justify="between"
-                align="start"
-                pad="medium"
-                background="light-1"
-                gap="large"
-                overflow="scroll">
-                    {this.state.recentProducts.map(product => 
-                      <Product/>
+                {/** 4 Most Recent Products */}
+                <Heading level="2" style={style.heading}>Most Recent Products</Heading>
+                <Box
+                    direction="row-responsive"
+                    justify="between"
+                    align="start"
+                    pad="medium"
+                    background="light-1"
+                    gap="large"
+                    overflow="scroll">
+                    {this.state.recentProducts.map(product =>
+                        <Product />
                     )}
-            </Box>
-            {/** All other products */}
-            <Heading level="2" style={style.heading}> Other products </Heading>
-            {/** Other product grid */}
-            <Grid
-                columns={{
-                    count: 4,
-                    size: "auto"
-                }}
-                gap="medium"
-                margin="small">
-                    {this.state.otherProducts.map(product =>
-                    <Product/>
-                    )}
-            </Grid>
+                </Box>
+                {/** All other products */}
+                <Heading level="2" style={style.heading}> Other products </Heading>
+                {/** Other product grid */}
+                <ResponsiveGrid
+                    rows={rows}
+                    columns={columns}
+                    gap="small"
+                    areas={areas}
+                    margin="medium">
+                        {this.state.otherProducts.map(product =>
+                            <Product/>
+                        )}
+                </ResponsiveGrid>
             </>
         )
     }
